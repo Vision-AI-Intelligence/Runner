6fa015cbe448782dbb0ec2942f6cd439726aef2d
Nguyễn Tuấn Kiệt <nguyentuankiet23031998@gmail.com>
2020-12-13 18:54:25 +0700

    Add ML Runner into main app

--- main.py
+++ main.py
@@ -1,26 +1,14 @@
-from typing import Optional
-
 from fastapi import FastAPI
 
-from runner.job import ioJob
 from runner.routers import io_router
+from runner.routers import ml_router
 
 app = FastAPI()
 
 app.include_router(io_router.router, prefix='/io')
+app.include_router(ml_router.router, prefix='/ml')
 
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
-
-
-@app.get("/add/{a}/{b}")
-def add(a: float, b: float):
-    result = ioJob.add.delay(a, b)
-    return result.get(timeout=1)
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
